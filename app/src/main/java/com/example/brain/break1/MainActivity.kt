7b490adb06279df6a6fcf1556ae9c0f78b4bfdd1
ChristianB <christian.benitez@epn.edu.ec>
2020-01-27 12:43:57 -0500

    Intent MainActivity - SetNameActivity

--- app/src/main/java/com/example/brain/break1/MainActivity.kt
+++ app/src/main/java/com/example/brain/break1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.brain.break1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,7 +8,10 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
 
-    
+    private lateinit var button_play : Button
+    private lateinit var button_scores : Button
+    private lateinit var button_settings : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -15,6 +19,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button_play = findViewById(R.id.btn_play)
+        button_scores = findViewById(R.id.btn_scores)
+        button_settings = findViewById(R.id.btn_options)
 
+        button_play.setOnClickListener{showNameActivity()}
+
+    }
+
+    private fun showNameActivity(){
+        val gameActivityIntent = Intent (this, SetNameActivity::class.java)
+        startActivity(gameActivityIntent)
     }
 }
---
+++ app/src/main/java/com/example/brain/break1/SetNameActivity.kt
@@ -0,0 +1,12 @@
+package com.example.brain.break1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetNameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_name)
+    }
+}
